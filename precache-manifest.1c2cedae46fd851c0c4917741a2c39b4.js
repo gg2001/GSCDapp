07010a5a7b4e3dffc56f22901adf02f8f7b96aba
Gautham Ganesh Elango <gautham.gg@gmail.com>
2020-10-22 22:21:55 +1100

    first

---
+++ precache-manifest.1c2cedae46fd851c0c4917741a2c39b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388f59da4e3b97fcf1e3e20d0f1a9f46",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2dbdd49fe538c6b82c18",
+    "url": "./static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "8a7b86d4fc6d36d1639b",
+    "url": "./static/css/main.3f37dc0e.chunk.css"
+  },
+  {
+    "revision": "2dbdd49fe538c6b82c18",
+    "url": "./static/js/2.52f8b585.chunk.js"
+  },
+  {
+    "revision": "7fdc43d9b31d8ee4c2bf92bab4c4eb8a",
+    "url": "./static/js/2.52f8b585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7b86d4fc6d36d1639b",
+    "url": "./static/js/main.0ff49de4.chunk.js"
+  },
+  {
+    "revision": "54af0916f5d47369bcc4",
+    "url": "./static/js/runtime-main.28645b20.js"
+  }
+]);
